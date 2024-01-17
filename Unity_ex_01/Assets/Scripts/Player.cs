0037988abaf8886ef828e894f3e18905bb113916
l11040 <113540683+l11040@users.noreply.github.com>
2024-01-17 19:08:49 +0900

    Update : 캐릭터 플레이

--- Unity_ex_01/Assets/Scripts/Player.cs
+++ Unity_ex_01/Assets/Scripts/Player.cs
@@ -6,12 +6,18 @@ public class Player : MonoBehaviour
 {
     public float speed;
     private Rigidbody rb;
+    public AudioSource itemSound;
+    public bool isJump;
 
+    public int score;
+
+    public int leftItem;
 
     void Start()
     {
         //같은 Inspector 내에서 Rigidbody를 가져오겠다는 뜻
         rb = GetComponent<Rigidbody>();
+        leftItem = 10;
     }
 
     private void OnTriggerEnter(Collider other)
@@ -20,6 +26,35 @@ public class Player : MonoBehaviour
         if (other.tag == "Item")
         {
             other.gameObject.SetActive(false);
+            itemSound.Play();
+            score = score + 10;
+        }
+
+        if (other.tag == "JumpItem")
+        {
+            other.gameObject.SetActive(false);
+            itemSound.Play();
+            score = score + 20;
+        }
+
+        leftItem--;
+
+        Debug.Log("score : " + score);
+        Debug.Log("left Item : " + leftItem);
+
+
+        if (score >= 120)
+        {
+            Debug.Log("Game End!!");
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            isJump = true;
         }
     }
 
@@ -37,5 +72,11 @@ public class Player : MonoBehaviour
 
         // 플레이어 힘을 가함
         rb.AddForce(movement * speed);
+
+        if (Input.GetKey(KeyCode.Space) && isJump)
+        {
+            rb.AddForce(Vector3.up * 3f, ForceMode.Impulse);
+            isJump = false;
+        }
     }
 }
